836dbce1d030e4f1d46e5b3cf5b5c0eb1fade97f
Tuhin Roy <troy0870@gmail.com>
2020-03-15 21:21:22 +0530

    add skill-test component with route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SkillTestComponent } from './skill-test/skill-test.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '', redirectTo: 'take-test', pathMatch: 'full'
+}, {
+  path: 'take-test', component: SkillTestComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
