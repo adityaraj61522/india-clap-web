51a8c662852ee312ac9bd32eb69d2abdb138ccd4
Tuhin Roy <troy0870@gmail.com>
2020-03-21 08:47:36 +0530

    add login-page component

--- src/app/professional-networking/professional-networking.module.ts
+++ src/app/professional-networking/professional-networking.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ProfessionalNetworkingRoutingModule } from './professional-networking-routing.module';
 import { ProfessionalNetworkingComponent } from './professional-networking.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 
 @NgModule({
-  declarations: [ProfessionalNetworkingComponent, HomePageComponent],
+  declarations: [ProfessionalNetworkingComponent, HomePageComponent, LoginPageComponent],
   imports: [
     CommonModule,
     ProfessionalNetworkingRoutingModule
