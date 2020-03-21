9d21b0392458cd93f7b17bba2a02edb0820aeaaf
Tuhin Roy <troy0870@gmail.com>
2020-03-21 08:49:31 +0530

    add reset-password-page component

--- src/app/professional-networking/professional-networking.module.ts
+++ src/app/professional-networking/professional-networking.module.ts
@@ -7,10 +7,11 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { ForgotPasswordPageComponent } from './forgot-password-page/forgot-password-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
 
 
 @NgModule({
-  declarations: [ProfessionalNetworkingComponent, HomePageComponent, LoginPageComponent, RegistrationPageComponent, ForgotPasswordPageComponent],
+  declarations: [ProfessionalNetworkingComponent, HomePageComponent, LoginPageComponent, RegistrationPageComponent, ForgotPasswordPageComponent, ResetPasswordPageComponent],
   imports: [
     CommonModule,
     ProfessionalNetworkingRoutingModule
