eb6de6f13147860ac26653e25311f98a5b5ab525
Tuhin Roy <troy0870@gmail.com>
2020-03-21 08:48:13 +0530

    add registration-page component

--- src/app/professional-networking/professional-networking.module.ts
+++ src/app/professional-networking/professional-networking.module.ts
@@ -5,10 +5,11 @@ import { ProfessionalNetworkingRoutingModule } from './professional-networking-r
 import { ProfessionalNetworkingComponent } from './professional-networking.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LoginPageComponent } from './login-page/login-page.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
 
 
 @NgModule({
-  declarations: [ProfessionalNetworkingComponent, HomePageComponent, LoginPageComponent],
+  declarations: [ProfessionalNetworkingComponent, HomePageComponent, LoginPageComponent, RegistrationPageComponent],
   imports: [
     CommonModule,
     ProfessionalNetworkingRoutingModule
