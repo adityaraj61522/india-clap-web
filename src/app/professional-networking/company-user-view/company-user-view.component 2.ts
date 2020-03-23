f8546e7bc7b47ca42674b8a469e5e74709fb7ad1
Tuhin Roy <troy0870@gmail.com>
2020-03-23 11:34:47 +0530

    add remaining screens

---
+++ src/app/professional-networking/company-user-view/company-user-view.component 2.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-user-view',
+  templateUrl: './company-user-view.component.html',
+  styleUrls: ['./company-user-view.component.css']
+})
+export class CompanyUserViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
