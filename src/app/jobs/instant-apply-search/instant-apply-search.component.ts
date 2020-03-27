fd125a6de858b07afdc9590aab00fca0471517f1
Tuhin Roy <troy0870@gmail.com>
2020-03-27 23:02:16 +0530

    add job search screen changes

--- src/app/jobs/instant-apply-search/instant-apply-search.component.ts
+++ src/app/jobs/instant-apply-search/instant-apply-search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { JobsService } from 'src/app/services/jobs.service';
 
 @Component({
   selector: 'app-instant-apply-search',
@@ -9,8 +9,10 @@ import { Router } from '@angular/router';
 })
 export class InstantApplySearchComponent implements OnInit {
 
-  instantApplyForm:FormGroup;
-  constructor(private fb: FormBuilder,private router:Router) {
+  instantApplyForm: FormGroup;
+  constructor(
+    private jobService: JobsService
+  ) {
     this.instantApplyForm = new FormGroup({
       salary: new FormControl("", Validators.required),
       location_State: new FormControl("", Validators.required),
@@ -18,24 +20,16 @@ export class InstantApplySearchComponent implements OnInit {
       starting_time: new FormControl("", Validators.required),
       end_time: new FormControl("", Validators.required),
     });
-   }
-   submit()
-   {
+  }
+  submit() {
     this.instantApplyForm.controls["salary"].markAsTouched();
     this.instantApplyForm.controls["location_State"].markAsTouched();
     this.instantApplyForm.controls["location_District"].markAsTouched();
     this.instantApplyForm.controls["starting_time"].markAsTouched();
     this.instantApplyForm.controls["end_time"].markAsTouched();
-    if(this.instantApplyForm.valid)
-    {
-      console.log(this.instantApplyForm.get('salary').value);
-      console.log(this.instantApplyForm.get('location_State').value);
-      console.log(this.instantApplyForm.get('location_District').value);
-      console.log(this.instantApplyForm.get('starting_time').value);
-      console.log(this.instantApplyForm.get('end_time').value);
-     
-    }
-   }
+    if (!this.instantApplyForm.valid) return;
+    this.jobService
+  }
 
   ngOnInit() {
   }
