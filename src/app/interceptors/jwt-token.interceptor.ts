59ae9324d9c0574e9a4b54e267baf7b36cdaef02
Tuhin Roy <troy0870@gmail.com>
2020-03-28 20:08:34 +0530

    add auth service file and jwt interceptor

---
+++ src/app/interceptors/jwt-token.interceptor.ts
@@ -0,0 +1,23 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class JwtTokenIntercelptor implements HttpInterceptor {
+
+    constructor(private authService: AuthService) { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let currentUser = this.authService.currentUserValue;
+        if (currentUser && currentUser.token) {
+            request = request.clone({
+                setHeaders: {
+                    Authorization: `Token ${currentUser.token}`
+                }
+            });
+        }
+        return next.handle(request)
+    }
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private currentUserSubject: BehaviorSubject<any>;
+
+  constructor() {
+    this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
+  }
+
+  public get currentUserValue() {
+    return this.currentUserSubject.value;
+  }
+
+}
